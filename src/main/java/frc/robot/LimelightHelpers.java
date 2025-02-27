369f71f6fa63a86f83e35e8ab6f274790647dcba
Galileo10695 <55943834+Galileo3034@users.noreply.github.com>
2025-02-27 20:20:16 +0200

    add

---
+++ src/main/java/frc/robot/LimelightHelpers.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class LimelightHelpers {
+
+    public static double getTX(String string) {
+        //TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTX'");
+    }
+
+}
