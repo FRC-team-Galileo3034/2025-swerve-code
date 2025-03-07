2c4f2f04d70a8d7094a86fbd162cfbed2946b142
razbarvaz1 <razbsa@gmail.com>
2025-03-07 16:38:05 +0200

    feeder and swerve no encoder

---
+++ src/main/java/frc/robot/commands/Shoulder/Shoulder.java
@@ -0,0 +1,10 @@
+package frc.robot.commands.Shoulder;
+
+public class Shoulder {
+
+    public void setVoltage(double d) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setVoltage'");
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/Elbow/ElbowConstens.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.Elbow;
+
+public class ElbowConstens {
+
+    public static final int ELBOW_ID = 10;
+    public static final double MOTOR_GEAR_RATIO = 1.0/5.0;
+}
